f654283888108cca5f28580c3cc4513eaa101eef
C23051986 <zhangh139@cardiff.ac.uk>
2024-07-03 17:12:18 +0100

    Initial commit

---
+++ src/main/java/com/example/teamatramshedtech/TeamATramshedTechApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamatramshedtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamATramshedTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamATramshedTechApplication.class, args);
+    }
+
+}
