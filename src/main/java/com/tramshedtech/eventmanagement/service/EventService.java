a9845d6d74fcdcaa1c2a3622d33eed03916a2332
c22049299 <narayanankuttyn@cardiff.ac.uk>
2024-07-18 21:05:05 +0100

    Add EventService interface and implementation class

---
+++ src/main/java/com/tramshedtech/eventmanagement/service/EventService.java
@@ -0,0 +1,13 @@
+package com.tramshedtech.eventmanagement.service;
+
+import com.tramshedtech.eventmanagement.entity.Event;
+
+import java.util.List;
+
+public interface EventService {
+    List<Event> getAllEvents();
+
+    Event getEventById(Long id);
+
+}
+
