431ba91f5e1c41fcf5ae09c9ce393a52833b2557
Snigdha Banani <snigdha.banani05@gmail.com>
2024-07-18 14:34:44 +0100

    Added initial code for Partipants and Speakers Page
    Signed-off-by: Snigdha Banani <snigdha.banani05@gmail.com>

---
+++ src/main/java/com/tramshedtech/eventmanagement/mapper/ParticipantSpeakerMapper.java
@@ -0,0 +1,16 @@
+package com.tramshedtech.eventmanagement.mapper;
+
+import com.tramshedtech.eventmanagement.entity.ParticipantSpeaker;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface ParticipantSpeakerMapper {
+    List<ParticipantSpeaker> getAll();
+    ParticipantSpeaker getById(@Param("id") int id);
+    boolean insert(ParticipantSpeaker participantSpeaker);
+    boolean update(ParticipantSpeaker participantSpeaker);
+    boolean delete(@Param("id") int id);
+}
