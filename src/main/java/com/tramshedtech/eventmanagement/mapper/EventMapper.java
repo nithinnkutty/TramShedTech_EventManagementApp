35223e9295b0355f4e806c9f1cca4982bc42bc87
c22049299 <narayanankuttyn@cardiff.ac.uk>
2024-07-19 10:18:02 +0100

    Update event mapper

--- src/main/java/com/tramshedtech/eventmanagement/mapper/EventMapper.java
+++ src/main/java/com/tramshedtech/eventmanagement/mapper/EventMapper.java
@@ -1,17 +1,15 @@
 package com.tramshedtech.eventmanagement.mapper;
 
 import com.tramshedtech.eventmanagement.entity.Event;
-import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 @Mapper
 public interface EventMapper {
 
-    @Select("SELECT * FROM events")
     List<Event> getAllEvents();
 
-    @Select("SELECT * FROM events WHERE id = #{id}")
     Event getEventById(Long id);
 
 }
