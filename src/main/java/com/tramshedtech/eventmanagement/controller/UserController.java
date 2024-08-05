e0b08859bf0973f16889e0559d6372db9dca2a9c
Haoxuan Zhang <zhangh139@cardiff.ac.uk>
2024-08-06 07:43:40 +0800

    Completed the front-end page for adding users to the user system

--- src/main/java/com/tramshedtech/eventmanagement/controller/UserController.java
+++ src/main/java/com/tramshedtech/eventmanagement/controller/UserController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
