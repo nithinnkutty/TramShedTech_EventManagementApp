c7ea3f8acc16ee7ad8653313ca4d4859d75b21ea
Shuoyan Zhao <zhaos31@cardiff.ac.uk>
2024-08-16 02:50:54 +0800

    [#]Change the comment to English.

--- src/test/java/com/tramshedtech/eventmanagement/BookingControllerTests.java
+++ src/test/java/com/tramshedtech/eventmanagement/BookingControllerTests.java
@@ -27,7 +27,7 @@ public class BookingControllerTests {
     private MockMvc mockMvc;
 
     /**
-     * 查询所有房间
+     * Find all rooms
      */
     @Test
     void testSearchAll() throws Exception {
@@ -56,13 +56,13 @@ public class BookingControllerTests {
 
     @Test
     void testDeleteBooking() throws Exception {
-        // 准备请求体的数据
+        // Prepare the data for the request body
         Map<String, Integer> requestData = new HashMap<>();
         requestData.put("id", 39);
-        // 将Map转换为JSON字符串
+        // Convert the Map to a JSON string
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonRequestData = objectMapper.writeValueAsString(requestData);
-        // 执行测试
+        // Perform the test
         mockMvc.perform(MockMvcRequestBuilders.post("/booking/delete")
                         .content(jsonRequestData)
                         .contentType(MediaType.APPLICATION_JSON)
@@ -74,7 +74,7 @@ public class BookingControllerTests {
     }
     @Test
     void testUpdateBooking() throws Exception {
-        // 准备请求体的数据
+        // Prepare the data for the request body
         Map<String, Object> bookingData = new HashMap<>();
         bookingData.put("id", 2);
         bookingData.put("roomNumber", "Room 2");
@@ -89,11 +89,11 @@ public class BookingControllerTests {
         bookingData.put("img", "Barry Goodsheds template.png");
         bookingData.put("status", 1);
 
-        // 将Map转换为JSON字符串
+        // Convert the Map to a JSON string
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonBookingData = objectMapper.writeValueAsString(bookingData);
 
-        // 执行测试
+        // Perform the test
         mockMvc.perform(MockMvcRequestBuilders.post("/booking/update")
                         .content(jsonBookingData)
                         .contentType(MediaType.APPLICATION_JSON)
@@ -105,14 +105,14 @@ public class BookingControllerTests {
     }
     @Test
     void updateStatus() throws Exception {
-        // 准备请求体的数据
+        // Prepare the data for the request body
         Map<String, Integer> requestData = new HashMap<>();
         requestData.put("id", 4);
         requestData.put("status", 0);
-        // 将Map转换为JSON字符串
+        // Convert the Map to a JSON string
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonRequestData = objectMapper.writeValueAsString(requestData);
-        // 执行测试
+        // Perform the test
         mockMvc.perform(MockMvcRequestBuilders.post("/booking/updateStatus")
                         .content(jsonRequestData)
                         .contentType(MediaType.APPLICATION_JSON)
@@ -124,14 +124,14 @@ public class BookingControllerTests {
     }
 
     /**
-     * 新增
+     * add test
      *
      * @throws Exception
      */
 
     @Test
     void testAddBooking() throws Exception {
-        // 准备请求体的数据
+        // Prepare the data for the request body
         Map<String, Object> bookingData = new HashMap<>();
         bookingData.put("roomNumber", "111");
         bookingData.put("roomName", "111");
@@ -145,10 +145,10 @@ public class BookingControllerTests {
         bookingData.put("img", "2016101910270126.jpg");
         bookingData.put("startDate", "2024-08-13T16:00:00.000Z");
         bookingData.put("endDate", "2024-09-10T16:00:00.000Z");
-        // 将Map转换为JSON字符串
+        // Convert the Map to a JSON string
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonBookingData = objectMapper.writeValueAsString(bookingData);
-        // 执行测试
+        // Perform the test
         mockMvc.perform(MockMvcRequestBuilders.post("/booking/add")
                         .content(jsonBookingData)
                         .contentType(MediaType.APPLICATION_JSON)
@@ -160,24 +160,24 @@ public class BookingControllerTests {
     }
 
     /**
-     * 测试文件上传
+     * Test file upload
      *
      * @throws Exception
      */
     @Test
     void testUploadFile() throws Exception {
-        // 创建模拟文件
+        // Create mock file
         MockMultipartFile file = new MockMultipartFile(
-                "file", // 文件名
-                "test-image.jpg", // 客户端文件名
-                "image/jpeg", // MIME类型
-                "Hello, World!".getBytes() // 文件内容
+                "file", // file name
+                "test-image.jpg", // Client file name
+                "image/jpeg", // MIME type
+                "Hello, World!".getBytes() // file content
         );
 
-        // 准备期望的响应结果
+        // Prepare the desired response results
         Map<String, String> expectedResponse = new HashMap<>();
         expectedResponse.put("message", "File uploaded successfully");
-        // 执行测试
+        // Perform the test
         mockMvc.perform(multipart("/booking/upload")
                         .file(file)
                         .accept(MediaType.APPLICATION_JSON))
