cf64cd0cb15c09e164523349ad7b464e05574559
c22049299 <narayanankuttyn@cardiff.ac.uk>
2024-08-16 14:42:45 +0100

    Refractor BookingControllerTests

--- src/test/java/com/tramshedtech/eventmanagement/controller/BookingControllerTests.java
+++ src/test/java/com/tramshedtech/eventmanagement/controller/BookingControllerTests.java
@@ -1,4 +1,4 @@
-package com.tramshedtech.eventmanagement;
+package com.tramshedtech.eventmanagement.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
